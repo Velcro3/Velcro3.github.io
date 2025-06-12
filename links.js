ee1c2e3c2e26748dbd72c6e8a372f6d0d4c407f2
viewer <leoncatpl27272@proton.me>
2025-06-12 18:30:25 +0100

    Finish link to YouTube

--- links.js
+++ links.js
@@ -1,3 +1,13 @@
+let ytModal = document.getElementById("ytmodal")
 function openHome() {
     window.location.href = "index.html"
+}
+function ytModalOpen() {
+    ytModal.classList = "promptOpen"
+}
+function gotoYt() {
+    window.location.href = "https://youtube.com/@viewer_the_twelth"
+}
+function cancelYt() {
+    ytModal.classList = "prompt"
 }
