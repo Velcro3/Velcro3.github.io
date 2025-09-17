ba27114a38641cb0d9b7445b22ee1d55761d52f9
Velcro3 <93869119+Velcro3@users.noreply.github.com>
2025-09-17 17:50:16 +0100

    Update projects.js
    Fix prettyproject button

--- projects.js
+++ projects.js
@@ -7,3 +7,7 @@ function openMore() {
 function openHome() {
     window.location.href = "index.html"
 }
+function prettyProject() {
+    window.location.href = "https://github.com/velcro3/prettyproject"
+}
+
